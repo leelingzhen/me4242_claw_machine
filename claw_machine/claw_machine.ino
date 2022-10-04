11af17a9b1073e9e608f5e47995ec4cca4e68584
leelingzhen <leeling.zhenn+github@gmail.com>
2022-10-04 14:14:23 +0800

    first commit

---
+++ claw_machine/claw_machine.ino
@@ -0,0 +1,16 @@
+
+#define SIGNAL_PIN 6
+
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(SIGNAL_PIN, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(SIGNAL_PIN, HIGH);
+  delay(1000);
+  digitalWrite(SIGNAL_PIN, LOW);
+  delay(1000);
+}
---
+++ fluidic_control_board/fluidic_control_board.ino
@@ -0,0 +1,54 @@
+       //Pin Assigement 
+#define VALVE_CTRL_0  3
+#define VALVE_CTRL_1  9
+#define VALVE_CTRL_2  10
+#define VALVE_CTRL_3  11
+#define PUMP_CTRL     5
+#define PRESSURE_0    A0
+#define PRESSURE_1    A1
+#define PRESSURE_2    A2
+#define PRESSURE_3    A3
+#define SIGNAL_PIN 6
+
+//define pump ON/OFF control
+#define PUMP_ON   digitalWrite(PUMP_CTRL, HIGH);
+#define PUMP_OFF  digitalWrite(PUMP_CTRL, LOW); 
+
+//User Variables
+int PRESSURE[4] ={0};
+int pump_signal = 0;
+
+void setup() {
+  Serial.begin(115200);
+  pinMode(VALVE_CTRL_0, OUTPUT);
+  pinMode(VALVE_CTRL_1, OUTPUT);
+  pinMode(VALVE_CTRL_2, OUTPUT);
+  pinMode(VALVE_CTRL_3, OUTPUT);
+  pinMode(PUMP_CTRL, OUTPUT);
+  VALVE_OFF(VALVE_CTRL_0);
+  VALVE_OFF(VALVE_CTRL_1);
+  VALVE_OFF(VALVE_CTRL_2);
+  VALVE_OFF(VALVE_CTRL_3);
+  PUMP_OFF;
+
+  //signal pin to be communicated by other arduino
+  pinMode(SIGNAL_PIN,INPUT);
+}
+
+void loop() {
+  pump_signal = digitalRead(SIGNAL_PIN);
+  digitalWrite(PUMP_CTRL, pump_signal);
+}
+
+void VALVE_ON(byte VALVE_INDEX){
+  digitalWrite(VALVE_INDEX, HIGH);
+}
+void VALVE_OFF(byte VALVE_INDEX){
+  digitalWrite(VALVE_INDEX, LOW);
+}
+void READ_PRESSURE(){
+  PRESSURE[0]=analogRead(PRESSURE_0);
+  PRESSURE[1]=analogRead(PRESSURE_1);
+  PRESSURE[2]=analogRead(PRESSURE_2);
+  PRESSURE[3]=analogRead(PRESSURE_3);
+}
