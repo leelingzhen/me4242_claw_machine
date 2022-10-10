98b8c25a8a68e7c56212a01582928a0eaec1fc66
leelingzhen <leeling.zhenn+github@gmail.com>
2022-10-10 11:36:56 +0800

    some backbone code to get started

--- claw_machine/claw_machine.ino
+++ claw_machine/claw_machine.ino
@@ -14,3 +14,27 @@ void loop() {
   digitalWrite(SIGNAL_PIN, LOW);
   delay(1000);
 }
+
+
+
+double [] joystick_output() {
+/* function to get the joystick positions should return a double array of [x_mappings,y_mappings]
+values taken from analog input should be divded by 1024
+*/
+}
+
+void move_x(double gain) {
+//moving claw machine in x direction with gain taken from joystick output
+//should function consider killswitch?
+
+}
+
+void move_z(double gain) {
+//same as move_x
+
+}
+
+void claw_actuate(int signal) {
+//to close or open the claw of the machine
+  digitalWrite(SIGNAL_PIN, signal)
+}
