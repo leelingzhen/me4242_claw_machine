87c2dee8212b33706325099136ec9404895e5a60
Mridul Mahesh <mridulmahesh262@gmail.com>
2022-10-18 11:03:34 +0800

    added ystepper motor code

--- claw_machine/bt_XYaxis.ino
+++ claw_machine/bt_XYaxis.ino
@@ -3,7 +3,10 @@
 // Adjust based on the steps movement
 // Button must kill the joystick
 // Define pin connections
-AccelStepper myStepper(1, 7, 6);
+// 2 inputs: step and direction
+AccelStepper xStepper(1, 7, 6);
+AccelStepper yStepper(2, 3, 4);
+// Joystick Controls
 const int VRx = A8;
 const int VRy = A9;
 const int SW = 5;
@@ -19,36 +22,33 @@ int yMap = 0;
 void setup() {
   // set the maximum speed, acceleration factor,
   // initial speed and the target position
-  myStepper.setMaxSpeed(600);
-  // myStepper.setAcceleration(50);
-  myStepper.setSpeed(200);
+  xStepper.setMaxSpeed(600);
+  xStepper.setSpeed(200);
+  yStepper.setMaxSpeed(600);
+  yStepper.setSpeed(200);
   Serial.begin(115200);
 }
 
 void loop() {
   xValue = analogRead(VRx);
   yValue = analogRead(VRy);
-  Serial.print(xValue);
-  Serial.print("\t");
-  Serial.print(yValue);
-  Serial.print("\t");
+  // Serial.print(xValue);
+  // Serial.print("\t");
+  // Serial.print(yValue);
+  // Serial.print("\t");
   xMap = map(xValue,0,1023,-3,3);
   yMap = map(yValue,0,1023,-3,3);
   if (xMap == 1 or xMap == -1) {
     xMap=0;
   }
+  if (yMap == 1 or yMap == -1) {
+    yMap=0;
+  }
   // Move the motor one step
-  myStepper.setSpeed(200*xMap);
-  myStepper.runSpeed();
+  xStepper.setSpeed(200*xMap);
+  xStepper.runSpeed();
+  yStepper.setSpeed(200*xMap);
+  yStepper.runSpeed();
   Serial.print('2');
   Serial.print("\t");
 }
-
-/*
-- Coin input
-- Limit switch (to kill in each direction)
-- Servo motor (scissor mechanism)
-- Stepper Motor - arm up and down (done)
-- Fluidic Interface with other arduino (done)
-- Button press flow: reading the button input, send to fluidic board to grab, servo up, motor going to home position
-*/
