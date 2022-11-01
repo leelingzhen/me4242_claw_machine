c35c7d38e883f62f09f9db00fb0a90ce732d7ed7
Mridul Mahesh <mridulmahesh262@gmail.com>
2022-11-01 15:37:24 +0800

    updated button servo flow

--- claw_machine/claw_machine.ino
+++ claw_machine/claw_machine.ino
@@ -3,6 +3,7 @@
 #include "StepperManager.h"
 #include <Servo.h>
 
+// As of 1st November: 1430microseconds or 86 is stop for servo
 void arm_retract(Servo servo, int extend_angle, int retract_angle, int servo_step_delay) {
   int servo_pos = extend_angle;
   for (int i = 0; i <= extend_angle - retract_angle; i++) {
@@ -10,6 +11,8 @@ void arm_retract(Servo servo, int extend_angle, int retract_angle, int servo_ste
     delay(servo_step_delay);
   }
 }
+
+
 
 // Adjust based on the steps movement
 // Button must kill the joystick
@@ -23,10 +26,36 @@ const int SIGNAL_PIN = 8;
 const int SERVO_PIN = 9;
 const int BUTTON_PIN = 10;
 
-//servo settings
-const int servo_step_delay = 10;
-const int retract_angle = 100; //90 will be max, completely horizontal
-const int extend_angle = 170;// 180 will be max, completely vertical
+// To be reset when coin is input
+bool buttonPressed = false;
+
+int player_button(){
+  //returns 1 if the button has been pressed and zero if the button has been released
+  int button_state = digitalRead(BUTTON_PIN);
+  return button_state;
+}
+
+
+void claw_actuate(int signal) {
+//to close or open the claw of the machine
+//send a signal of HIGH to close the claw and a signal of LOW to release the claww
+  digitalWrite(SIGNAL_PIN, signal);
+}
+
+void claw_move_height(Servo servo, int direction , int duration = 1000, int stop = 1430){
+  servo.writeMicroseconds(direction);
+  Serial.println(direction);
+  delay(1000);
+  Serial.println("delay");
+  servo.writeMicroseconds(stop);
+  Serial.println("stop");
+  delay(1000);
+  servo.writeMicroseconds(2000);
+  Serial.println(2000);
+  delay(1000);
+  Serial.println("delay");
+  servo.writeMicroseconds(stop);
+}
 
 void setup() {
   // set the maximum speed, acceleration factor,
@@ -39,12 +68,17 @@ void setup() {
 
   //servo setup
   servo_arm.attach(SERVO_PIN);
+  // servo_arm.write(86);
+  // servo_arm.writeMicroseconds(1430);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   stepperMotors.moveMotor(200);
-  
+  if(!buttonPressed && player_button()){
+    claw_move_height(servo_arm, 1000);
+    buttonPressed = true;
+  }
 }
 
 
@@ -70,24 +104,4 @@ void loop() {
 //   delay(sequence_delay);
 //   claw_actuate(0);
 // }
-
-
-int player_button(){
-  //returns 1 if the button has been pressed and zero if the button has been released
-  int button_state = digitalRead(BUTTON_PIN);
-  return button_state;
-}
-
-
-void claw_actuate(int signal) {
-//to close or open the claw of the machine
-//send a signal of HIGH to close the claw and a signal of LOW to release the claww
-  digitalWrite(SIGNAL_PIN, signal);
-}
-
-void claw_move_height(Servo servo, int direction , int duration = 1000, int stop = 1440){
-  servo.writeMicroseconds(direction);
-  delay(1000);
-  servo.writeMiroseconds(stop);
-}
 
